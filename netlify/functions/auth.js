3fcacb92350a405cb81ae803662508864e1b9e00
Nick <67593147+iNiKKo@users.noreply.github.com>
2025-10-20 20:08:40 +0100

    Create auth.js

---
+++ netlify/functions/auth.js
@@ -0,0 +1,12 @@
+export async function handler(event, context) {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      message: 'Auth function ready',
+    }),
+  };
+}
